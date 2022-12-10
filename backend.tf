381886927877039f0ba1db18b8f5ebffba6ac142
Shu Ambat <shadab.ambat@aktsk.jp>
2022-12-10 21:54:51 +0900

    adding template backend

---
+++ backend.tf
@@ -0,0 +1,12 @@
+// Uncomment the following resource block to enable saving state to GCS
+// Remember to replace the Bucket ID and other attributes as needed!
+
+/*
+terraform {
+  backend "gcs" {
+    bucket = "<REPLACE_WITH_YOUR_BUCKET_NAME>"
+    prefix = "terraform/state"
+  }
+}
+*/
+
